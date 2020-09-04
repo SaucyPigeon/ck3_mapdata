a5cbf20c0430b95dec7914b012c3acc4897eb673
warior4356 <warior4356@yahoo.com>
2020-09-03 20:00:26 -0700

    Added counties, kingdoms, and empires

--- parser.py
+++ parser.py
@@ -66,20 +66,27 @@ def parse_special():
 
 def parse_titles():
     titles_tree = pyradox.txt.parse_file(
-        # 'C:\Program Files (x86)\Steam\steamapps\common\Crusader Kings III\game\history\\titles\\00_other_titles.txt',
         'C:\Program Files (x86)\Steam\steamapps\common\Crusader Kings III\game\common\landed_titles\\00_landed_titles.txt',
         game="Stellaris")
 
     for key_e in list(titles_tree.keys()):
         if key_e[:2] == "e_":
             titles[key_e] = {}
-            colors[key_e] = list(titles_tree[key_e].find_all('color'))
+            color = list(titles_tree[key_e].find_all('color'))
+            if type(color[0]) == pyradox.datatype.Color:
+                colors[key_e] = list(color[0].to_rgb())
+            else:
+                colors[key_e] = color
             capitals[key_e] = titles_tree[key_e]["capital"]
 
             for key_k in list(titles_tree[key_e].keys()):
                 if key_k[:2] == "k_":
                     titles[key_e][key_k] = {}
-                    colors[key_k] = list(titles_tree[key_e][key_k].find_all('color'))
+                    color = list(titles_tree[key_e][key_k].find_all('color'))
+                    if type(color[0]) == pyradox.datatype.Color:
+                        colors[key_k] = list(color[0].to_rgb())
+                    else:
+                        colors[key_k] = color
                     capitals[key_k] = titles_tree[key_e][key_k]["capital"]
 
                     for key_d in list(titles_tree[key_e][key_k].keys()):
@@ -103,43 +110,62 @@ def parse_titles():
                                             titles[key_e][key_k][key_d][key_c][key_b] = \
                                                 titles_tree[key_e][key_k][key_d][key_c][key_b]["province"]
 
-    # title_file = open('C:\Program Files (x86)\Steam\steamapps\common\Crusader Kings III\game\common\landed_titles\\00_landed_titles.txt', "r", encoding='utf8')
-    # titulars = True
-    # empire = "none"
-    # kingdom = "none"
-    # dutchy = "none"
-    # county = "none"
-    # barony = "none"
-    # for line in title_file:
-    #     if(line[:8] == "########"):
-    #         titulars = False
-    #
-    #     if(titulars):
-    #         continue
-    #
-    #     if(line[:2] == 'e_'):
-    #         # print(line)
-    #         empire = line.split()[0]
-    #         titles[empire] = {}
-    #     elif(line[:3] == '\tk_'):
-    #         # print(line)
-    #         kingdom = line.split()[0]
-    #         titles[empire][kingdom] = {}
-    #     elif(line[:4] == '\t\td_'):
-    #         # print(line)
-    #         dutchy = line.split()[0]
-    #         titles[empire][kingdom][dutchy] = {}
-    #     elif(line[:5] == '\t\t\tc_'):
-    #         # print(line)
-    #         county = line.split()[0]
-    #         titles[empire][kingdom][dutchy][county] = {}
-    #     elif(line[:6] == '\t\t\t\tb_'):
-    #         # print(line)
-    #         barony = line.split()[0]
-    #         titles[empire][kingdom][dutchy][county][barony] = 0
-    #     elif(line[:13] == '\t\t\t\t\tprovince'):
-    #         titles[empire][kingdom][dutchy][county][barony] = line.split()[2]
 
+def print_counties():
+    outfile = open("counties.txt", "w", encoding='utf8')
+    outfile.write('{| class="wikitable sortable mw-collapsible" style="text-align: left;"\n'
+                  '|+ Counties\n'
+                  '|-\n'
+                  '! rowspan="2" | County\n'
+                  '! rowspan="2" | [[Dutchies|Dutchy]]\n'
+                  '! rowspan="2" | [[Kingdoms|Kingdom]]\n'
+                  '! rowspan="2" | [[Empires|Empire]]\n'
+                  '! rowspan="2" | [[Barony|Baronies]]\n'
+                  '! colspan="2" | [[Development]]\n'
+                  '! rowspan="2" | [[Barony#Buildings|Special Buildings/Slots]]\n'
+                  '! rowspan="2" | ID\n'
+                  '|-\n'
+                  '! 867\n'
+                  '! 1066\n')
+    for empire in titles.keys():
+        for kingdom in titles[empire].keys():
+            for dutchy in titles[empire][kingdom].keys():
+                for county in titles[empire][kingdom][dutchy].keys():
+                    county_dev_867 = 0
+                    if empire in dev_867:
+                        county_dev_867 = dev_867[empire]
+                    if kingdom in dev_867:
+                        county_dev_867 = dev_867[kingdom]
+                    if dutchy in dev_867:
+                        county_dev_867 = dev_867[dutchy]
+                    if county in dev_867:
+                        county_dev_867 = dev_867[county]
+
+                    county_dev_1066 = 0
+                    if empire in dev_1066:
+                        county_dev_1066 = dev_1066[empire]
+                    if kingdom in dev_1066:
+                        county_dev_1066 = dev_1066[kingdom]
+                    if dutchy in dev_1066:
+                        county_dev_1066 = dev_1066[dutchy]
+                    if county in dev_1066:
+                        county_dev_1066 = dev_1066[county]
+
+                    baronies = 0
+                    special_buildings = 0
+                    for barony in titles[empire][kingdom][dutchy][county].keys():
+                        baronies = baronies + 1
+                        barony_id = int(titles[empire][kingdom][dutchy][county][barony])
+                        if(barony_id in list(special.keys()) and special[barony_id] > 0):
+                            special_buildings = special_buildings + special[barony_id]
+
+                    outfile.write('|-\n!style = "background-color:rgb({},{},{})" | {}\n | {} || {} || '
+                                      '{} || align="right"| {} || align="right"| {} || align="right"| {} || '
+                                  'align="right"| {} || {}\n'.format(
+                        colors[county][0], colors[county][1], colors[county][2], names[county], names[dutchy],
+                        names[kingdom], names[empire], baronies, county_dev_867, county_dev_1066, special_buildings,
+                        county))
+    outfile.write("|}")
 
 def print_dutchies():
     outfile = open("dutchies.txt", "w", encoding='utf8')
@@ -147,10 +173,11 @@ def print_dutchies():
                   '|+ De Jure Duchies\n'
                   '|-\n'
                   '! rowspan="2" | Duchy\n'
-                  '! rowspan="2" | [[Kingdoms|Kingdom]]*\n'
-                  '! rowspan="2" | [[Empires|Empire]]*\n'
-                  '! rowspan="2" | [[Provinces|Counties]]*\n'
-                  '! rowspan="2" | Baronies*\n'
+                  '! rowspan="2" | [[Kingdoms|Kingdom]]\n'
+                  '! rowspan="2" | [[Empires|Empire]]\n'
+                  '! rowspan="2" | [[Provinces|Counties]]\n'
+                  '! rowspan="2" | Baronies\n'
+                  '! rowspan="2" | Largest County\n'
                   '! colspan="4" | Developments\n'
                   '! rowspan="2" | Special Buildings\n'
                   '! rowspan="2" | ID\n'
@@ -212,20 +239,88 @@ def print_dutchies():
                     if local_baronies > largest_county:
                         largest_county = local_baronies
 
+                capital = "None"
+                if capitals[dutchy] in names.keys():
+                    capital = names[capitals[dutchy]]
                 outfile.write('|-\n!style = "background-color:rgb({},{},{})" | {}\n | {} || {} || '
-                              'align="right"| {} || align="right"| {} || align="right"| {} || '
+                              'align="right"| {} || align="right"| {} ||  align="right"| {} ||align="right"| {} || '
                               'align="right"| {} || align="right"| {} || align="right"| {} || align="right"| '
                               '{} || {} || {}\n'.format(colors[dutchy][0], colors[dutchy][1], colors[dutchy][2],
                                                         names[dutchy], names[kingdom], names[empire], counties,
-                                                        baronies, total_dev_867, highest_dev_867, total_dev_1066,
-                                                        highest_dev_1066, special_buildings, dutchy, capitals[dutchy]))
+                                                        baronies, largest_county, total_dev_867, highest_dev_867,
+                                                        total_dev_1066, highest_dev_1066, special_buildings, dutchy,
+                                                        capital))
+    outfile.write("|}")
+
+def print_kingdoms():
+    outfile = open("kingdom.txt", "w", encoding='utf8')
+    outfile.write('{| class="wikitable sortable mw-collapsible" style="text-align: left;"\n'
+                  '|+ De Jure Kingdoms\n'
+                  '|-\n'
+                  '! rowspan="2" | Kingdom\n'
+                  '! rowspan="2" | [[duchies|Dejure duchies]]\n'
+                  '! rowspan="2" | [[Empires|Empire]]\n'
+                  '! colspan="2" | [[County|Counties]]\n'
+                  '! rowspan="2" | ID\n'
+                  '! rowspan="2" | Capital\n')
+    for empire in titles.keys():
+        for kingdom in titles[empire].keys():
+            dejure_dutchies = []
+            counties = 0
+            for dutchy in titles[empire][kingdom].keys():
+                dejure_dutchies.append(names[dutchy])
+                for county in titles[empire][kingdom][dutchy].keys():
+                    counties = counties + 1
+
+            capital = "None"
+            if capitals[kingdom] in names.keys():
+                capital = names[capitals[kingdom]]
+
+            outfile.write('|-\n!style = "background-color:rgb({},{},{})" | {}\n | {} || {} || '
+                              'align="right"| {} || {} || {}\n'.format(
+                colors[kingdom][0], colors[kingdom][1], colors[kingdom][2], names[kingdom], ', '.join(dejure_dutchies),
+                names[empire], counties, kingdom, capital))
+    outfile.write("|}")
+
+def print_empires():
+    outfile = open("empires.txt", "w", encoding='utf8')
+    outfile.write('{| class="wikitable sortable mw-collapsible" style="text-align: left;"\n'
+                  '|+ De Jure Empires\n'
+                  '|-\n'
+                  '! rowspan="2" | Empire\n'
+                  '! rowspan="2" | [[kingdoms|Dejure Kingdoms]]\n'
+                  '! colspan="2" | [[Dutchies|Dutchies]]\n'                  
+                  '! colspan="2" | [[County|Counties]]\n'
+                  '! rowspan="2" | Special Conditions\n'
+                  '! rowspan="2" | ID\n'
+                  '! rowspan="2" | Capital\n')
+    for empire in titles.keys():
+        dejure_kingdoms = []
+        dutchies = 0
+        counties = 0
+        for kingdom in titles[empire].keys():
+            dejure_kingdoms.append(names[kingdom])
+            for dutchy in titles[empire][kingdom].keys():
+                dutchies = dutchies + 1
+                for county in titles[empire][kingdom][dutchy].keys():
+                    counties = counties + 1
+
+        capital = "None"
+        if capitals[empire] in names.keys():
+            capital = names[capitals[empire]]
+        outfile.write('|-\n!style = "background-color:rgb({},{},{})" | {}\n | {} || align="right"| {} || '
+                      'align="right"| {} ||  || {} || {}\n'.format(
+                       colors[empire][0], colors[empire][1], colors[empire][2], names[empire],
+                       ', '.join(dejure_kingdoms), dutchies, counties, empire, capital))
     outfile.write("|}")
 
 
 parse_titles()
-# print(colors)
 parse_dev()
 parse_special()
 parse_names()
+print_counties()
 print_dutchies()
+print_kingdoms()
+print_empires()
 
